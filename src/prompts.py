83624dd90b9bc1f49cae9d1411a108b379eda5c3
German Zuccoli <143474679+germanzuccoli@users.noreply.github.com>
2023-12-13 14:29:38 -0700

    Create prompts.py

---
+++ src/prompts.py
@@ -0,0 +1,8 @@
+prompts = [
+    "Call Flow: Analyze pcap for call flow, detailing entities/nodes recognized for protocol",
+    "Analyze PCAP: Identify failures, issues, anomalies. Focus on protocol ",
+    "Troubleshoot PCAP: Detail issues and recommendations for protocol ",
+    "Routing Issues: Examine pcap, highlight routing problems, suggest solutions for protocol ",
+    "Security: Assess PCAP for security concerns (unauthorized access, vulnerabilities), recommend improvements for protocol",
+    "Performance Review: Assess pcap for performance and latency, identify areas for improvement for protocol"
+]
