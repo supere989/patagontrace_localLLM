553e3179cc9cd8ad664dae87f760c030f925ca23
German Zuccoli <143474679+germanzuccoli@users.noreply.github.com>
2023-12-15 08:28:20 -0700

    Update patagontrace.py

--- patagontrace.py
+++ patagontrace.py
@@ -37,9 +37,9 @@ def run_tshark_cmd_and_process_result(cmd):
         print("Error running tshark command or tshark not found.")
         return ""
     result_text = ' '.join(result)
-    if len(result_text) > 5000:
-        result_text = result_text[:5000]
-        print("Trimming pcap text to 5000 characters for AI processing.")
+    if len(result_text) > 7000:
+        result_text = result_text[:7000]
+        print("Trimming pcap text to 7000 characters for AI processing.")
     return result_text
 
 def pcap_to_txt(input_file):
