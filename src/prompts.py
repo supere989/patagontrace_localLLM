956d2e5c1b9f51a0075ce28f38057b9e4f66ecc9
German Zuccoli <143474679+germanzuccoli@users.noreply.github.com>
2023-12-14 17:24:38 -0700

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -1,8 +1,8 @@
 prompts = [
-    "Call Flow: Analyze pcap for call flow, detailing entities/nodes related to",
-    "Analyze PCAP: Identify failures, issues, anomalies. Focus on ",
-    "Troubleshoot PCAP: Detail issues and recommendations related to",
-    "Routing Issues: Examine pcap, highlight routing problems, suggest solutions related to",
-    "Security: Assess PCAP for security concerns (unauthorized access, vulnerabilities), recommend improvements related to",
-    "Performance Review: Assess pcap for performance and latency, identify areas for improvement related to"
+    "Call Flow Analysis: Analyze the call flow from the pcap, detailing entities/nodes involved and scenarios, ",
+    "PCAP Analysis: Identify failures, issues, or anomalies present in the pcap, ",
+    "PCAP Troubleshooting: Analyze the pcap by detailing issues and providing recommendations, ",
+    "Routing Issues Examination: Examine the pcap for routing problems and suggest solutions, ",
+    "Security Assessment: Assess the pcap for security concerns, such as unauthorized access or vulnerabilities, and recommend improvements, ",
+    "Performance Review: Evaluate the pcap for performance metrics and latency, identifying areas for improvement, "
 ]
